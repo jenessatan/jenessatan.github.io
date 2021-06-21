b5053bef6c7455fe6930d90576a9ee3e66fd138c
jenessa <jenessaxtan@gmail.com>
2021-06-21 14:17:07 -0700

    initialize gastby site with barebones hello-world starter

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # jenessatan.github.io
+A simple developer portfolio to showcase projects.
+### Summer 2021 Portfolio Redesign
+Complete overhaul of previous site, moving away from vanilla HTML, CSS & Javascript. This site also serves as a way to learn Gatsby and improve frontend design skills.
---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [],
+  siteMetadata: {
+    title: "Jenessa Tan Portfolio",
+    description: "Software Developer Portfolio",
+    copyright: "This website is copyright 2021 Jenessa Tan",
+    author: "JT"
+  }
+}
